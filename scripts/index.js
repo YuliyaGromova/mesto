8cf06b460f5938fb0ad428c16ef0f5cd73d6a843
Юленька <chervyak_9@mail.ru>
2022-12-19 23:19:42 +0300

    style: форматирование и оптимизация картинок

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,36 @@
 function popupCloseOpen() {
-    editPopup.classList.toggle('popup_opened');
-    editName.value=userName.textContent;
-    editInfo.value=userInfo.textContent;
+  editPopup.classList.toggle("popup_opened");
+  editName.value = userName.textContent;
+  editInfo.value = userInfo.textContent;
 }
 
-let editPopup = document.querySelector('.popup');
-let toggleButton = editPopup.querySelector('.popup__toggle')
-let profile = document.querySelector('.profile__user');
-let editButton = profile.querySelector('.profile__button-edit');
+let editPopup = document.querySelector(".popup");
+let toggleButton = editPopup.querySelector(".popup__toggle");
+let profile = document.querySelector(".profile__user");
+let editButton = profile.querySelector(".profile__button-edit");
 
-let userName=profile.querySelector('.profile__name');
-let editName=editPopup.querySelector('.popup__item_el_name');
+let userName = profile.querySelector(".profile__name");
+let editName = editPopup.querySelector(".popup__item_el_name");
 
-let userInfo=profile.querySelector('.profile__info');
-let editInfo=editPopup.querySelector('.popup__item_el_info');
-
+let userInfo = profile.querySelector(".profile__info");
+let editInfo = editPopup.querySelector(".popup__item_el_info");
 
 // кнопка "редактировать" открывает popup
-editButton.addEventListener('click', popupCloseOpen);
+editButton.addEventListener("click", popupCloseOpen);
 // кнопка "закрыть" в popup закрывает его
-toggleButton.addEventListener('click', popupCloseOpen);
+toggleButton.addEventListener("click", popupCloseOpen);
 
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
+  let nameInput = editName.value;
+  let infoInput = editInfo.value;
 
-    let nameInput = editName.value;
-    let infoInput = editInfo.value;
-
-    userName.textContent = nameInput;
-    userInfo.textContent = infoInput;
-    popupCloseOpen();
+  userName.textContent = nameInput;
+  userInfo.textContent = infoInput;
+  popupCloseOpen();
 }
 
-const editSave=document.querySelector('.popup');
+const editSave = document.querySelector(".popup");
 
-editSave.addEventListener('submit', handleFormSubmit); 
+editSave.addEventListener("submit", handleFormSubmit);
