7c67cd5195a5ed689242424f1b02cc13060a379d
Юленька <chervyak_9@mail.ru>
2022-12-20 23:58:50 +0300

    fix: испарвление замечаний

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,25 @@
-function popupCloseOpen() {
-  editPopup.classList.toggle("popup_opened");
+let editPopup = document.querySelector('.popup');
+let toggleButton = editPopup.querySelector('.popup__toggle');
+let profile = document.querySelector('.profile__user');
+let editButton = profile.querySelector('.profile__button-edit');
+
+let userName = profile.querySelector('.profile__name');
+let editName = editPopup.querySelector('.popup__item_el_name');
+
+let userInfo = profile.querySelector('.profile__info');
+let editInfo = editPopup.querySelector('.popup__item_el_info');
+
+const editSave = editPopup.querySelector('.popup__container');
+
+function popupOpen() {
+  editPopup.classList.add('popup_opened');
   editName.value = userName.textContent;
   editInfo.value = userInfo.textContent;
 }
 
-let editPopup = document.querySelector(".popup");
-let toggleButton = editPopup.querySelector(".popup__toggle");
-let profile = document.querySelector(".profile__user");
-let editButton = profile.querySelector(".profile__button-edit");
-
-let userName = profile.querySelector(".profile__name");
-let editName = editPopup.querySelector(".popup__item_el_name");
-
-let userInfo = profile.querySelector(".profile__info");
-let editInfo = editPopup.querySelector(".popup__item_el_info");
-
-// кнопка "редактировать" открывает popup
-editButton.addEventListener("click", popupCloseOpen);
-// кнопка "закрыть" в popup закрывает его
-toggleButton.addEventListener("click", popupCloseOpen);
+function popupClose() {
+  editPopup.classList.remove('popup_opened');
+}
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -28,9 +29,13 @@ function handleFormSubmit(evt) {
 
   userName.textContent = nameInput;
   userInfo.textContent = infoInput;
-  popupCloseOpen();
+  popupClose();
 }
 
-const editSave = document.querySelector(".popup");
+// кнопка "редактировать" открывает popup
+editButton.addEventListener('click', popupOpen);
+// кнопка "закрыть" в popup закрывает его
+toggleButton.addEventListener('click', popupClose);
 
-editSave.addEventListener("submit", handleFormSubmit);
+// кнопка "сохранить" в popup сохраняет введенные значения и закрывает popup
+editSave.addEventListener('submit', handleFormSubmit);
