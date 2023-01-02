c04aa2bd5339643d970ef2bda788bf0c766c6bf8
Юленька <chervyak_9@mail.ru>
2023-01-03 00:04:04 +0300

    feat: умолчательные карточки в галлерее

--- scripts/index.js
+++ scripts/index.js
@@ -39,3 +39,42 @@ toggleButton.addEventListener('click', popupClose);
 
 // кнопка "сохранить" в popup сохраняет введенные значения и закрывает popup
 editSave.addEventListener('submit', handleFormSubmit);
+
+// умолчательные карточки с фото
+
+const initialCards = [
+  {
+    name: 'Россия',
+    link: 'https://images.unsplash.com/photo-1612622807284-9854ea9bbdd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+  },
+  {
+    name: 'Греция',
+    link: 'https://images.unsplash.com/photo-1649609855494-51ccd16ac8d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    name: 'Объединенные Арабские Эмираты',
+    link: 'https://images.unsplash.com/photo-1642952238795-da915a83206d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+  },
+  {
+    name: 'Индия',
+    link: 'https://images.unsplash.com/photo-1609067643332-8785d998aee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    name: 'Испания',
+    link: 'https://images.unsplash.com/photo-1573393264565-b85b58325758?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    name: 'Мальдивы',
+    link: 'https://images.unsplash.com/photo-1649231144211-2840666bae26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+  }
+];
+
+const addCards = document.querySelector('.gallery__cards'); //куда вставить
+const template = document.querySelector('#gallery-card').content; // от куда вставлять
+for (let i=0; i<initialCards.length; i+=1) {
+  const cardTemplate = template.querySelector('.gallery__card').cloneNode(true); //клон 
+  cardTemplate.querySelector('.gallery__photo').src = initialCards[i].link;
+  cardTemplate.querySelector('.gallery__photo').alt = initialCards[i].name;
+  cardTemplate.querySelector('.gallery__name-place').textContent = initialCards[i].name;
+  addCards.append(cardTemplate);
+}
