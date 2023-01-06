d89438de37806719a67953a23227ad23d992375b
Юленька <chervyak_9@mail.ru>
2023-01-07 01:18:15 +0300

    Feat: пробелмы с гитом, хорошо хоть сохранила все перед мерджем

--- scripts/index.js
+++ scripts/index.js
@@ -1,128 +1,142 @@
-let editPopup= document.querySelector('#popupEdit');
-let toggleButton = editPopup.querySelector('.popup__toggle');
-let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.profile__button-edit');
+const profile = document.querySelector(".profile");
+const userName = profile.querySelector(".profile__name");
+const userInfo = profile.querySelector(".profile__info");
+const editButton = profile.querySelector(".profile__button-edit");
+const addButton = profile.querySelector(".profile__button-add");
 
-let userName = profile.querySelector('.profile__name');
-let editName = editPopup.querySelector('.popup__item_el_name');
+const addCards = document.querySelector(".gallery__cards");
+const template = document.querySelector("#gallery-card").content;
 
-let userInfo = profile.querySelector('.profile__info');
-let editInfo = editPopup.querySelector('.popup__item_el_info');
+const toggleButtons = document.getElementsByClassName("popup__toggle"); // псевдомассив из "крестиков"
+const buttonsSave = document.getElementsByClassName("popup__edit"); // псевдомассив из кнопок сохраняющих изменения
 
-const editSave = editPopup.querySelector('.popup__edit');
+// Массив с умолчательными карточками
+const initialCards = [
+  {
+    name: "Россия",
+    link: "https://images.unsplash.com/photo-1612622807284-9854ea9bbdd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+  },
+  {
+    name: "Греция",
+    link: "https://images.unsplash.com/photo-1649609855494-51ccd16ac8d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+  },
+  {
+    name: "Объединенные Арабские Эмираты",
+    link: "https://images.unsplash.com/photo-1642952238795-da915a83206d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "Индия",
+    link: "https://images.unsplash.com/photo-1609067643332-8785d998aee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Испания",
+    link: "https://images.unsplash.com/photo-1573393264565-b85b58325758?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+  },
+  {
+    name: "Мальдивы",
+    link: "https://images.unsplash.com/photo-1649231144211-2840666bae26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+];
 
-function popupOpen() {
-  editPopup.classList.add('popup_opened');
-  editName.value = userName.textContent;
-  editInfo.value = userInfo.textContent;
-};
+//функция открывающая popup
+function popupOpen(index) {
+  //если кликнута кнопка редактирования проофиля
+  if (index === "edit") {
+    document.querySelector("#popupEdit").classList.add("popup_opened");
 
-function popupClose() {
-  editPopup.classList.remove('popup_opened');
+    document.querySelector("[name='namePopup']").value = userName.textContent;
+    document.querySelector("[name='infoPopup']").value = userInfo.textContent;
+    //если кликнута кнопка добавления новой карточки
+  } else if (index === "add") {
+    document.querySelector("#popupAdd").classList.add("popup_opened");
+    document.querySelector("[name='namePlacePopup']").value = "";
+    document.querySelector("[name='linkPopup']").value = "";
+  } else {
+    alert("что то пошло не так");
+  }
 }
 
+function popupOpenPic(evt) {
+  const eventTargetPic = evt.target.parentElement;
+  document.querySelector("#popupOpenPic").classList.add("popup_opened");
+  document.querySelector(".popup__photo").src =
+    eventTargetPic.querySelector(".gallery__photo").src;
+  document.querySelector(".popup__subtitle").textContent =
+    eventTargetPic.querySelector(".gallery__name-place").textContent;
+}
+
+//функция закрывающая открытый popup
+function popupClose() {
+  document.querySelector(".popup_opened").classList.remove("popup_opened");
+}
+
+//функция сохраняющая введенные данные
 function handleFormSubmit(evt) {
   evt.preventDefault();
+  const eventTarget = evt.target;
+  if (eventTarget.name === "popupEdit") {
+    const nameInput = document.querySelector("[name='namePopup']").value;
+    const infoInput = document.querySelector("[name='infoPopup']").value;
+    //Введенные данные попали на страницу в раздел информации о пользователе
+    userName.textContent = nameInput;
+    userInfo.textContent = infoInput;
+  } else if (eventTarget.name === "popupAdd") {
+    const nameInput = document.querySelector("[name='namePlacePopup']").value;
+    const infoInput = document.querySelector("[name='linkPopup']").value;
 
-  let nameInput = editName.value;
-  let infoInput = editInfo.value;
-
-  userName.textContent = nameInput;
-  userInfo.textContent = infoInput;
+    //создается новая карточка
+    createCard(nameInput, infoInput);
+  }
   popupClose();
 }
 
-// кнопка "редактировать" открывает popup
-editButton.addEventListener('click', popupOpen);
-
-// кнопка "закрыть" в popup закрывает его
-toggleButton.addEventListener('click', popupClose);
-
-// кнопка "сохранить" в popup сохраняет введенные значения и закрывает popup
-editSave.addEventListener('submit', handleFormSubmit);
-
-// умолчательные карточки с фото
-const initialCards = [
-  {
-    name: 'Россия',
-    link: 'https://images.unsplash.com/photo-1612622807284-9854ea9bbdd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
-  },
-  {
-    name: 'Греция',
-    link: 'https://images.unsplash.com/photo-1649609855494-51ccd16ac8d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
-  },
-  {
-    name: 'Объединенные Арабские Эмираты',
-    link: 'https://images.unsplash.com/photo-1642952238795-da915a83206d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-  },
-  {
-    name: 'Индия',
-    link: 'https://images.unsplash.com/photo-1609067643332-8785d998aee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-  },
-  {
-    name: 'Испания',
-    link: 'https://images.unsplash.com/photo-1573393264565-b85b58325758?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
-  },
-  {
-    name: 'Мальдивы',
-    link: 'https://images.unsplash.com/photo-1649231144211-2840666bae26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-  }
-];
-
-const addCards = document.querySelector('.gallery__cards'); //куда вставить
-const template = document.querySelector('#gallery-card').content; // от куда вставлять
-for (let i=0; i<initialCards.length; i+=1) {
-  const cardTemplate = template.querySelector('.gallery__card').cloneNode(true); //клон 
-  cardTemplate.querySelector('.gallery__photo').src = initialCards[i].link;
-  cardTemplate.querySelector('.gallery__photo').alt = initialCards[i].name;
-  cardTemplate.querySelector('.gallery__name-place').textContent = initialCards[i].name;
-  addCards.append(cardTemplate);
-
+//лайки
+function likeDislike(evt) {
+  const eventTargetLike = evt.target;
+  eventTargetLike.classList.toggle("like_active");
 }
 
-
-// popup  добавление нового места
-let addPopup= document.querySelector('#popupAdd');
-let toggleButtonAdd = addPopup.querySelector('.popup__toggle');
-let addButton = profile.querySelector('.profile__button-add');
-
-let namePlace = addCards.querySelector('.gallery__name-place');
-let namePlaceNew = addPopup.querySelector('.popup__item_el_name');
-
-let linkPhoto = addCards.querySelector('.gallery__photo');
-let linkPhotoNew = addPopup.querySelector('.popup__item_el_info');
-
-const addSave = addPopup.querySelector('.popup__edit');
-
-function popupOpenTwo() {
-  addPopup.classList.add('popup_opened');
-  linkPhotoNew.value = '';
-  namePlaceNew.value = '';
- };
-
-function popupCloseTwo() {
-  addPopup.classList.remove('popup_opened');
-};
-
-function handleFormSubmitTwo(evt) {
-  evt.preventDefault();
-
-  let namePlaceInput = namePlaceNew.value;
-  let linkInput = linkPhotoNew.value;
-
-  const cardTemplate = template.querySelector('.gallery__card').cloneNode(true); //клон 
-  cardTemplate.querySelector('.gallery__photo').src = linkInput;
-  cardTemplate.querySelector('.gallery__photo').alt = namePlaceInput;
-  cardTemplate.querySelector('.gallery__name-place').textContent = namePlaceInput;
-  addCards.append(cardTemplate);
-
-  popupCloseTwo();
+//удаление карточки по "урне"
+function deleteCard(evt) {
+  const eventTargetDelete = evt.target.parentElement;
+  eventTargetDelete.remove();
+}
+//создание новой карточки
+function createCard(name, link) {
+  const cardTemplate = template.querySelector(".gallery__card").cloneNode(true); //клон
+  cardTemplate.querySelector(".gallery__photo").src = link;
+  cardTemplate.querySelector(".gallery__photo").alt = name;
+  cardTemplate.querySelector(".gallery__name-place").textContent = name;
+  addCards.prepend(cardTemplate);
+  cardTemplate.querySelector(".like").addEventListener("click", likeDislike);
+  cardTemplate
+    .querySelector(".gallery__delete")
+    .addEventListener("click", deleteCard);
+  cardTemplate
+    .querySelector(".gallery__photo")
+    .addEventListener("click", popupOpenPic);
 }
 
-// // кнопка "редактировать" открывает popup
-addButton.addEventListener('click', popupOpenTwo);
-// // кнопка "закрыть" в popup закрывает его
-toggleButtonAdd.addEventListener('click', popupCloseTwo);
+// умолчательные карточки с фото (отображаются при загрузке страницы)
+for (let i = 0; i < initialCards.length; i++) {
+  createCard(initialCards[i].name, initialCards[i].link);
+}
 
-// // кнопка "сохранить" в popup сохраняет введенные значения и закрывает popup
-addSave.addEventListener('submit', handleFormSubmitTwo);
+// кнопка "редактировать" открывает popup редактирования информации о пользователе
+editButton.addEventListener("click", () => {
+  popupOpen("edit");
+});
+// кнопка "добавить" открывает popup добавления новой картиочки
+addButton.addEventListener("click", () => {
+  popupOpen("add");
+});
+
+// кнопка "закрыть" в popup закрывает открытый popup (навешивание события на все крестики)
+for (let i = 0; i < toggleButtons.length; i++) {
+  toggleButtons[i].addEventListener("click", popupClose);
+}
+
+// кнопка "сохранить" в popup сохраняет введенные значения и закрывает popup (навешивание события на все кнопки сохраняющие изменения)
+for (let i = 0; i < buttonsSave.length; i++) {
+  buttonsSave[i].addEventListener("submit", handleFormSubmit);
+}
